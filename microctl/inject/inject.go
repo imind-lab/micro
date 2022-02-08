93dc2faaf22e3658026f1ebd7534b7a233447fa2
daniel <daniel.song@chope.co>
2022-02-09 05:35:14 +0800

    update response injection

---
+++ microctl/inject/inject.go
@@ -0,0 +1,118 @@
+package inject
+
+import (
+	"bufio"
+	"bytes"
+	"io"
+	"os"
+	"strings"
+)
+
+func Process(path string) ([]byte, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	var header bytes.Buffer
+	var body bytes.Buffer
+	var content bytes.Buffer
+
+	exist := false
+	headerEnd := false
+	tag := ""
+	reponse := ""
+
+	reader := bufio.NewReader(file)
+	for {
+		line, _, err := reader.ReadLine()
+		if err != nil {
+			if err == io.EOF {
+				break
+			} else {
+				panic(err)
+			}
+		}
+		temp := string(line)
+		if strings.Contains(temp, "import (") {
+			headerEnd = true
+			header.Write(line)
+			header.WriteString("\n")
+		} else if strings.Contains(temp, "// @inject_tag") {
+			tag = temp
+
+			body.Write(line)
+			body.WriteString("\n")
+		} else if strings.Contains(temp, "// @inject_response") {
+			exist = true
+			reponse = temp
+
+			body.Write(line)
+			body.WriteString("\n")
+		} else {
+			if len(tag) > 0 {
+				items := strings.Split(tag, " ")
+				if len(items) >= 3 {
+					new := "omitempty\" " + items[2]
+					tmp := strings.Replace(temp, "omitempty\"", new, 1)
+					body.WriteString(tmp)
+				} else {
+					body.Write(line)
+				}
+				body.WriteString("\n")
+				tag = ""
+			} else if len(reponse) > 0 {
+				body.Write(line)
+				body.WriteString("\n")
+
+				if temp == "}" {
+					body.WriteString("\n")
+					body.WriteString(reponse)
+
+					items := strings.Split(reponse, " ")
+					if len(items) >= 3 {
+						body.WriteString("\nfunc (x *" + items[2] + ") SetCode(code status.Code) {\n\tx.Code = int32(code)\n\tx.Msg = code.String()\n}\n")
+					}
+					if len(items) >= 5 {
+						body.WriteString("\nfunc (x *" + items[2] + ") SetBody(code status.Code, " + items[4] + " " + items[3] + ") {\n\tx.Code = int32(code)\n\tx.Msg = code.String()\n\tx." + strings.Title(items[4]) + " = " + items[4] + "\n}\n")
+					}
+					reponse = ""
+				}
+			} else {
+				if headerEnd {
+					body.Write(line)
+					body.WriteString("\n")
+				} else {
+					header.Write(line)
+					header.WriteString("\n")
+				}
+			}
+		}
+	}
+
+	content.Write(header.Bytes())
+	if exist {
+		content.WriteString("\t\"github.com/imind-lab/micro/status\"\n")
+	}
+	content.Write(body.Bytes())
+
+	return content.Bytes(), nil
+}
+
+func Save(path string, content []byte) error {
+	file, err := os.OpenFile(path, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0666)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	writer := bufio.NewWriter(file)
+	_, err = writer.Write(content)
+	if err != nil {
+		return err
+	}
+	writer.Flush()
+
+	return nil
+}
---
+++ status/status.go
@@ -0,0 +1,113 @@
+package status
+
+import (
+	"fmt"
+)
+
+// A Code will be returned
+// whenever an SMSMessage is sent.
+type Code int
+
+// String implements the fmt.Stringer interface
+func (c Code) String() string {
+	return Errors[c]
+}
+
+// General error code configuration
+// Possible response codes
+const (
+	Success Code = iota + 1000
+	Throttled
+	MissingParams
+	InvalidParams
+	InvalidCredentials
+	InternalError
+	InvalidMessage
+	RESTNotEnabled
+	CommunicationFailed
+	InvalidSignature
+	InvalidCharactersInMessage
+	InvalidLanguageType
+	InvalidUrl
+	InvalidIPAddress
+	InvalidHTTPStatusCode
+	EmptyReturnValue
+	InvalidJSONFormat
+	PullDataFailed
+	SendToQueueFailed
+	DBConnectFailed
+	DBQueryFailed
+	DBSaveFailed
+	DBNoRowsAffected
+	RouteNotFound
+	RecordNotExist
+	SystemError
+	OtherErrors
+)
+
+var Errors = map[Code]string{
+	Success:                    "Success",
+	Throttled:                  "Throttled",
+	MissingParams:              "Missing params",
+	InvalidParams:              "Invalid params",
+	InvalidCredentials:         "Invalid credentials",
+	InternalError:              "Internal error",
+	InvalidMessage:             "Invalid message",
+	RESTNotEnabled:             "Account not enabled for REST",
+	CommunicationFailed:        "Communication failed",
+	InvalidSignature:           "Invalid signature",
+	InvalidCharactersInMessage: "Invalid characters in message",
+	InvalidLanguageType:        "Invalid language type",
+	InvalidUrl:                 "Invalid URL",
+	InvalidIPAddress:           "Invalid IP address",
+	InvalidHTTPStatusCode:      "Invalid HTTP status code",
+	EmptyReturnValue:           "Empty return value",
+	InvalidJSONFormat:          "Invalid JSON format",
+	PullDataFailed:             "Pull data failed",
+	SendToQueueFailed:          "Send to queue failed",
+	DBConnectFailed:            "DB connect failed",
+	DBQueryFailed:              "DB query failed",
+	DBSaveFailed:               "DB Save failed",
+	DBNoRowsAffected:           "DB No rows affected",
+	RouteNotFound:              "Route not found",
+	RecordNotExist:             "Record not found",
+	SystemError:                "System error",
+	OtherErrors:                "Unknown error",
+}
+
+type Error struct {
+	Code Code
+	Msg  string
+}
+
+func New(code Code) Error {
+	return Error{
+		Code: code,
+		Msg:  code.String(),
+	}
+}
+
+// Error 实现Error接口
+func (err Error) Error() string {
+	return fmt.Sprintf("code = %v, message = %v", err.Code, err.Msg)
+}
+
+// ResponseOK 处理成功
+var ResponseOK = New(Success)
+
+// ErrorDBConnect 数据库相关问题
+var ErrorDBConnect = New(DBConnectFailed)
+var ErrorDBQuery = New(DBQueryFailed)
+var ErrorDBSave = New(DBSaveFailed)
+var ErrorDBNoRowsAffected = New(DBNoRowsAffected)
+
+// ErrorInvalidUrl 业务逻辑相关问题
+var ErrorInvalidUrl = New(InvalidUrl)
+var ErrorEmptyReturnValue = New(EmptyReturnValue)
+var ErrorPullDataFailed = New(PullDataFailed)
+var ErrorRouteNotFound = New(RouteNotFound)
+var ErrorInvalidParams = New(InvalidParams)
+var ErrorRecordNotExist = New(RecordNotExist)
+
+// ErrorSystem 严重错误
+var ErrorSystem = New(SystemError)
